a888255c542a635f3805eb551605c69c15b0ae59
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-04-04 16:34:16 +0200

    Adds motorcycles to the mix

---
+++ ParkingSpots/ParkingSpots/Motorcycle.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkingSpots
+{
+    public class MotorCycle
+    {
+        public string Color { get; set; }
+        public string LicencePlate { get; set; }
+    }
+}
--- ParkingSpots/ParkingSpots/Program.cs
+++ ParkingSpots/ParkingSpots/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ParkingSpots;
+
 var carList = new List<Car>();
 
 var redCar = new Car { Color = "Red", LicencePlate = "1234L" };
@@ -11,4 +12,13 @@ carList.Add(blueCar);
 for (int i = 0; i < carList.Count; i++)
 {
     Console.WriteLine(carList[i] + " car with licence plate " + carList[i].LicencePlate);
+}
+
+var motorcycleList = new List<MotorCycle>();
+motorcycleList.Add(new MotorCycle { Color = "Yellow", LicencePlate = "A-1357" });
+motorcycleList.Add(new MotorCycle { Color = "Green", LicencePlate = "B-2468" });
+
+for (int i = 0; i < motorcycleList.Count; i++)
+{
+    Console.WriteLine(motorcycleList[i] + " car with licence plate " + motorcycleList[i]);
 }
