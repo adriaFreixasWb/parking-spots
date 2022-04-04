489dfaaf3c6cf9e7ec0bc447a0c6d95aa3b8e1fa
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2022-04-04 17:13:02 +0200

    Add vehicle type

--- ParkingSpots/ParkingSpots/Car.cs
+++ ParkingSpots/ParkingSpots/Car.cs
@@ -4,6 +4,11 @@
     {
         public string Color { get; set; }
         public string LicencePlate { get; set; }
+        public VehicleType VehicleType { get; }
 
+        public Car()
+        {
+            VehicleType = VehicleType.Car;
+        }
     }
 }
--- ParkingSpots/ParkingSpots/Motorcycle.cs
+++ ParkingSpots/ParkingSpots/Motorcycle.cs
@@ -4,5 +4,11 @@
     {
         public string Color { get; set; }
         public string LicencePlate { get; set; }
+        public VehicleType VehicleType { get; }
+
+        public MotorCycle()
+        {
+            VehicleType = VehicleType.Motorcycle;
+        }
     }
 }
